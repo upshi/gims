a7394fc554cb7d4d4a029fe18b7d7a071fd1137b
upshi <827743366@qq.com>
2016-08-23 16:39:18 +0800

    初始导入

---
+++ gims-common/src/main/java/com/yiheidaodi/App.java
@@ -0,0 +1,13 @@
+package com.yiheidaodi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
